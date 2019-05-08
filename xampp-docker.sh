411263887101dbf2de7b4997ea852f6ffcf4a4b7
o-sen <github@informatique-et-numerique.org>
2019-05-08 18:13:49 +0200

    added X11 display on host

--- xampp-docker.sh
+++ xampp-docker.sh
@@ -2,14 +2,18 @@ IMAGE_NAME=xampp
 CONTAINER_NAME=xampp-docker
 PUBLIC_WWW_DIR='~/web_pages'
 
+echo "==========================================================================================="
+echo "= First execution : create a container named  '$CONTAINER_NAME' with X11 display on host"
+echo "= Subequent executions : start the container named : '$CONTAINER_NAME'"
+echo "==========================================================================================="
 
-echo "Running container '$CONTAINER_NAME' from image '$IMAGE_NAME'..."
-
-docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
-	echo "Creating new container..."
+docker start $CONTAINER_NAME > /dev/null 2> /dev/null && echo "Starting container" || {
+	echo "Creating new container named : '$CONTAINER_NAME' ...From image named : '$IMAGE_NAME' "
 	docker run \
 	       --detach \
 	       --tty \
+	       -e DISPLAY=$DISPLAY \
+            -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 	       -p 8086:80 \
 	       -p 3386:3306 \
 	       --name $CONTAINER_NAME \
@@ -18,10 +22,9 @@ docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
 }
 
 if [ "$#" -eq  "0" ]; then
-	docker exec --interactive --tty $CONTAINER_NAME bash
+	docker exec  --interactive --tty $CONTAINER_NAME bash
 elif [ "$1" = "stop" ]; then
 	docker stop $CONTAINER_NAME
 else
 	docker exec $CONTAINER_NAME $@
 fi
-
